b3c95dd0a106ec6175b1b0f90f3a243c8b20febd
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2019-11-14 22:54:16 -0300

    redo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
+  
 import React, { useState } from 'react';
+import { Route } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -11,8 +15,9 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path="/" render={props=> <SavedList list={savedList} {...props} />} />
+      <Route exact path="/" render={props => <MovieList />}/>
+      <Route path="/movies/:id" render={props => <Movie {...props} />}/>
     </div>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const SavedList = props => (
-  <div className="saved-list">
+const SavedList = props => {
+  const goToHome = () => {props.history.push("/")}
+  return (
+    <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div onClick={goToHome} className="home-button">Home</div>
   </div>
-);
+  )
+};
 
 export default SavedList;
